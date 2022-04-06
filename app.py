89b7d3b758ed82adb7144a3ee851deaaf009ff43
Karl <karln371@gmail.com>
2022-04-06 14:48:16 -0700

    no-person shooter

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-name = input('What is your name? ')
-fav_color = input('What is your favorite color? ')
-print(name + ' likes ' + fav_color)
+secret_word = "pow"
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = input('Take your shot pardner! ')
+    guess_count += 1
+    if guess == secret_word:
+        print("Nice shootin'")
+        break
+    elif guess_count != guess_limit:
+        print('you missed that one, try another!')
+print("That's all, hoss!")
